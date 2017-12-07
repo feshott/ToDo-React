a89a464da2f33ca1d05eda6dcd3ba1f34f2f6763
feshott <feshot@yandex.ru>
2017-12-07 15:47:22 +0300

    add delete btn

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -3,30 +3,46 @@ import Item from '../item/Item';
 import './body.css';
 
 export default class Body extends Component {
+
   constructor(props){
     super(props)
     this.state={
-        items:[ ],
-        procent:'0%'
+        items:[ ]
     }
-}
+  } 
   
+
+
   addItem = () => {
     let myInput =document.querySelector('.inputText')
     let text = { 
       idBig: Date.now(),
       name : myInput.value,
-      done: false
+      done: false,
+      
     } 
     myInput.value = ''
     let arr = this.state.items
     arr.push(text) 
     this.setState({
       items: arr
-    },this.newProcent())
+    })
   }
+  
+  delItem = (val) => {
 
+    console.log('hello i`m delete')
 
+    let arrThree = this.state.items
+    arrThree = arrThree.filter((el,i)=>{
+      if(el.idBig !== val ){
+        return el
+      }
+    })
+      this.setState({
+        items : arrThree
+      })
+  }
 
   testFunc = (val) =>{
     let arrTwo = this.state.items
@@ -36,36 +52,38 @@ export default class Body extends Component {
       }})
     this.setState({
       items:arrTwo,
-    },this.newProcent())
+    })
   }
 
-  newProcent =()=>{
-    let allTrue = 0 
-    let arrTwo = this.state.items
-    arrTwo.map((el,i)=>{
-      if(el.done == true){
-        allTrue++
-      }
-    })
-    let result = Math.round((allTrue*100)/arrTwo.length)
-    this.setState({
-      procent: result + '%'
-    })
-  }
 
   render() {
+    let procent = (()=>{
+      let allTrue = 0 
+      let arrTwo = this.state.items
+      arrTwo.map((el,i)=>{
+        if(el.done == true){
+          allTrue++
+        }
+      })
+    let result = Math.round((allTrue*100)/arrTwo.length)
+    return result
+    })()
+  
+    
     return (
       <div className="body">
 
         <div className="list">
 
-          {this.state.items.map((el,i)=><Item appThis={this}  data={el} key={i} testFunc={this.testFunc}/>)}
+          {this.state.items.map((el,i)=><Item appThis={this}  data={el} key={el.idBig} delItem={this.delItem} testFunc={this.testFunc}/>)}
+
         </div>
 
         <button onClick={this.addItem}>click me</button>
 
         <input className='inputText'  type="text"/>
-        <div className='procent'>{this.state.procent}</div>     
+
+        <div className='procent'>{procent || 0}%</div>     
         
       </div>
     );
--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -13,6 +13,8 @@ export default class Item extends Component {
         <input className='checkBox' onClick={()=>{this.props.testFunc(this.props.data.idBig)}} type='checkbox'/>
 
         <p>{this.props.data.name}</p>
+
+        <button className='delete_button' onClick={()=>{this.props.delItem(this.props.data.idBig)}}>delete</button>
         
       </div>
     );
